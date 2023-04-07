d77563c36cd39a538596dd3c6a34e20daf62fe63
Douglas Santos <dvieiras@aol.com>
2023-04-07 20:40:26 -0300

    update

--- got/run.sh
+++ got/run.sh
@@ -3,7 +3,7 @@
 GIT_DIR=$(find $HOME -name "git" -type d)
 
 findGitFile() {
-   find $GIT_DIR -name $1
+   find $GIT_DIR -name $1 -type f
 }
 
 findDir() {
@@ -15,20 +15,19 @@ createSymlink () {
 }
 
 mkdir $HOME/.clones
+mkdir $HOME/.i3
 sudo mkdir -p /usr/local/share/lombok
 
-sudo pacman -S plasma mpv wget xorg-xinit xorg-server nvidia xf86-video-amdgpu npm
-echo -e "export DESKTOP_SESSION=plasma\nexec startplasma-x11" >> $HOME/.xinitrc
+sudo pacman -S i3 xorg xorg-xinit neovim tmux alacritty ttf-dejavu \ 
+   mpv nvidia xf86-video-amdgpu npm atril deepin-image-viewer deepin-screenshot \ 
+   openjdk17-doc java17-openjfx maven stack haskell-language-server stack \ 
+   docker docker-compose fish tree mariadb dbeaver lazygit wget uvicorn unzip \
+   obs-studio code github-cli neofetch --noconfirm 
+echo "exec i3" >> $HOME/.xinitrc
 
-while read package; do
-   yes | sudo pacman -S "$package"
-done < packages/os_packages.txt
-while read package; do
-   pip install "$package"
-done < packages/pip_packages.txt
-while read package; do
-   sudo npm i -g "$package"
-done < packages/npm_packages.txt
+pip install mariadb mypy telebot yfinance python-binance 
+sudo npm i -g neovim pyright sass node-fetch 
+
 while read repo; do
    dir="$(echo "$repo" | awk -F '/' '{print $NF}')"
    git clone https://github.com/"$repo" "$HOME/.clones/$dir"
@@ -38,7 +37,6 @@ git clone https://aur.archlinux.org/google-chrome.git $HOME/.clones/google-chrom
 (cd $HOME/.clones/google-chrome && yes | makepkg -si)
 
 yes | sh $HOME/.clones/fzf/install
-yes | sh $HOME/.clones/kde_onedark/install
 curl -fLo ~/.vim/plug/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo wget https://projectlombok.org/downloads/lombok.jar \
@@ -52,6 +50,7 @@ createSymlink ".vimrc" "$HOME"
 createSymlink ".gitconfig" "$HOME"
 createSymlink "coc-settings.json" "$HOME/.vim"
 createSymlink "config.fish" "$HOME/.config/fish"
+createSymlink "config" "$HOME/.i3"
 
 # Fish functions link
 for fun in $(findDir "functions")/*; do
@@ -64,8 +63,3 @@ nvim -u ~/.vimrc -c "PlugInstall" -c "sleep 15" \
 nvim -u ~/.vimrc \ 
    -c "CocInstall coc-tsserver coc-java coc-json coc-pyright coc-git coc-sh coc-html coc-css coc-snippets coc-vimlsp coc-texlab" \
    -c "sleep 35" -c "q!" -c "q!"
-
-# Packages
-# pacman -Qent | grep -o '^[^ ]*' > packages/os_packages.txt
-# pip list --user | awk '{print $1}' > packages/pip_packages.txt
-# npm list -g --depth=0 | grep -o '^[^@]*' | awk -F/ '{print $NF}' > packages/npm_packages.txt
