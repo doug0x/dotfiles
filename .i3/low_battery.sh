04286c6151e1a77667cd938d0ef77d7e40c9e450
Douglas Santos <dvieiras@aol.com>
2023-04-23 03:44:34 -0300

    SAVE HER!!!

--- .i3/low_battery.sh
+++ .i3/low_battery.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+check_charging() {
+   while true; do
+      charging=$(cat /sys/class/power_supply/BAT0/status)
+      sleep 5
+      if [ "$charging" == "Charging" ]; then
+         pkill mpv
+         sleep 10
+         break
+      fi
+      sleep 5
+   done
+}
+
 while true; do
    battery=$(cat /sys/class/power_supply/BAT0/capacity)
 
    if [ "$battery" -le "7" ]; then
-      mpv ~/.i3/sinking.mp3
+      mpv ~/.i3/sinking.mp3 & check_charging
       battery_update=$(cat /sys/class/power_supply/BAT0/capacity)
-      if [ "$battery_update" -ge "7" ]; then
+      if [ "$battery_update" -ge "6" ]; then
          continue
       else
         sudo systemctl poweroff
