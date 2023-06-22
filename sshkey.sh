636f07e4355320ad57675c585ecf23d9b8c4e55c
Douglas Santos <dvieiras@aol.com>
2023-06-22 11:56:22 -0300

    update sshkey.sh

--- sshkey.sh
+++ sshkey.sh
@@ -2,6 +2,10 @@
 # ssh public key script
 echo -n "Enter your github email "
 read email
+
+echo -n "Pastebin api key "
+read key
+
 if [ -f "$HOME/.ssh/id_rsa.pub" ]; then
    cat $HOME/.ssh/id_rsa.pub
    echo -e "\n ====> https://github.com/settings/ssh/new \n"
@@ -10,5 +14,8 @@ else
    eval "$(ssh-agent -s)"
    ssh-add $HOME/.ssh/id_rsa
    cat $HOME/.ssh/id_rsa.pub
+   echo -e "\n\n" && curl -X POST -H "Content-Type: application/x-www-form-urlencoded" \
+      -d "api_dev_key=$key&api_option=paste&api_paste_code=$(urlencode < $HOME/.ssh/id_rsa.pub)&api_paste_private=1" \ 
+      https://pastebin.com/api/api_post.php && echo -e "\n\n"
    echo -e "\n ====> https://github.com/settings/ssh/new \n"
 fi
