d7318af3d0b78c7c841fea8a5751e2a628c0f792
Douglas Santos <dvieiras@aol.com>
2023-08-02 03:49:17 -0300

    fix: sshkey

--- sshkey.sh
+++ sshkey.sh
@@ -8,6 +8,9 @@ read key
 
 if [ -f "$HOME/.ssh/id_rsa.pub" ]; then
    cat $HOME/.ssh/id_rsa.pub
+   echo -e "\n\n" && curl -X POST -H "Content-Type: application/x-www-form-urlencoded" \ 
+      -d "api_dev_key=$key&api_option=paste&api_paste_code=$(< $HOME/.ssh/id_rsa.pub)&api_paste_private=1" \ 
+      https://pastebin.com/api/api_post.php && echo -e "\n\n"
    echo -e "\n ====> https://github.com/settings/ssh/new \n"
 else
    ssh-keygen -t rsa -b 4096 -C $email
@@ -15,7 +18,7 @@ else
    ssh-add $HOME/.ssh/id_rsa
    cat $HOME/.ssh/id_rsa.pub
    echo -e "\n\n" && curl -X POST -H "Content-Type: application/x-www-form-urlencoded" \
-      -d "api_dev_key=$key&api_option=paste&api_paste_code=$(urlencode < $HOME/.ssh/id_rsa.pub)&api_paste_private=1" \ 
+      -d "api_dev_key=$key&api_option=paste&api_paste_code=$(< $HOME/.ssh/id_rsa.pub)&api_paste_private=1" \ 
       https://pastebin.com/api/api_post.php && echo -e "\n\n"
    echo -e "\n ====> https://github.com/settings/ssh/new \n"
 fi
