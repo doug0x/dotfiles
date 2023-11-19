0ecc8f41c6261d728c21cd26feb3a334712915f7
Douglas Santos <dvieiras@aol.com>
2023-11-19 10:36:37 -0300

    update low_bat

--- .i3/low_battery.sh
+++ .i3/low_battery.sh
@@ -3,9 +3,9 @@
 check_charging() {
    while true; do
       charging=$(cat /sys/class/power_supply/BAT0/status)
-      sleep 5
+      sleep 2
       if [ "$charging" == "Charging" ]; then
-         pkill mpv
+         pkill -f mpv
          break
       fi
       sleep 5
@@ -17,7 +17,7 @@ while true; do
    if [ "$battery" -le "7" ]; then
       mpv $HOME/.i3/sinking.mp3 & check_charging
       battery_update=$(cat /sys/class/power_supply/BAT0/capacity)
-      if [ "$battery_update" -ge "6" ]; then
+      if [ "$battery_update" -ge "5" ]; then
          sleep 200
          continue
       else
