c8ecadb6a7931f9a8aa52370b7b91fe974f40736
me <dvieiras@aol.com>
2024-05-09 17:51:22 -0300

    update

--- .i3/script.sh
+++ .i3/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-GIT_DIR=$(find $HOME -name "toolazy" -type d)
+GIT_DIR=$(find $HOME -name "denv" -type d)
 
 findGitFile() {
    find $GIT_DIR -name $1 -type f
 }
 
-feh --bg-fill $(findGitFile "see.jpg")
+#feh --bg-fill $(findGitFile "see.jpg")
 xinput set-prop 13 "libinput Tapping Enabled" 1
 xinput set-prop 17 "libinput Tapping Enabled" 1
 picom
